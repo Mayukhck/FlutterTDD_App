1196f8dc4ed22b174f3a30ef95cbbd4a12ac8519
Mayukhck <mayukhck@gmail.com>
2024-05-07 11:00:45 +0530

    03

--- lib/demogorouter/navigator.dart
+++ lib/demogorouter/navigator.dart
@@ -1,9 +1,22 @@
 import 'package:go_router/go_router.dart';
 import 'package:shopping_list/widgets/grocery_list.dart';
+import 'package:shopping_list/widgets/new_item.dart';
+
+enum Screen {
+  newItem,
+  groceryItem
+}
 
 final goRouter = GoRouter(initialLocation: '/', routes: [
   GoRoute(
     path: '/',
     builder: (context, state) => const GroceryList(),
+    routes: [
+      GoRoute(
+        path: 'newItem',
+        name: Screen.newItem.name,
+        builder: (context, state) => const NewItem(),
+      ),
+    ],
   )
 ]);
